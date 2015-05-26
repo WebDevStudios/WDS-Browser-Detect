b8b3537c8d3d76aebc591e9e4f89d15c74836d77
Greg Rickaby <gregrickaby@gmail.com>
2015-05-26 11:02:41 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# WDS-Browser-Detect
+##WDS Browser Detect
 A lightweight plugin to detect browsers, operating systems, and devices.
+
+Powered by a (slightly) modified version of Chris Schuld's [https://github.com/cbschuld/Browser.php](https://github.com/cbschuld/Browser.php)
+
+## Installation
+* Download, unzip, and place into you WordPress `/plugins/` directory.
+* Activate via the WordPress plugin manager.
+
+## Example Usage
+
+These helper functions are intended to be used in conditionals and return either `TRUE` or `FALSE`. Basic usage looks like:
+
+```php
+if ( wds_browser_is_ie() ) {
+	echo 'The browser is IE';
+}
+```
+You can even detect browser versions for Chrome, Safari, Firefox, and IE:
+
+```php
+if ( wds_browser_is_ie( '9' ) ) {
+	echo 'The browser is IE 9';
+}
+```
+How about detecting if the browser is mobile?
+
+```php
+if ( wds_browser_is_mobile() ) {
+	echo 'The browser is mobile';
+}
+```
+...Or a tablet?
+
+```php
+if ( wds_browser_is_tablet() ) {
+	echo 'The browser is a tablet';
+}
+```
+...Why not both?
+
+```php
+if ( wds_browser_is_handheld() ) {
+	echo 'The browser is a handheld device';
+}
+```
+
+**A real world usage example:**
+
+```php
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function wds_client_body_classes( $classes ) {
+
+	if ( wds_browser_is_ie() ) {
+		$classes[] = 'ie';
+	}
+
+	if ( wds_browser_is_ie( '9' ) ) {
+		$classes[] = 'ie9';
+	}	
+
+	if ( wds_browser_is_mobile() ) {
+		$classes[] = 'phone';
+	}
+
+	if ( wds_browser_is_tablet() ) {
+		$classes[] = 'tablet';
+	}	
+
+	return $classes;
+}
+add_filter( 'body_class', 'wds_client_body_classes' );
+```
+For more examples, checkout the source code!
---
+++ wds-browser-detect.php
@@ -0,0 +1,315 @@
+<?php
+/**
+ * Plugin Name: WebDevStudios Browser Detect
+ * Plugin URI: http://webdevstudios.com
+ * Description: A lightweight plugin to detect browsers, operating systems, and devices.
+ * Author: WebDevStudios
+ * Author URI: http://webdevstudios.com
+ * Version: 1.0.0
+ * License: GPLv2
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Include our detection class
+if ( file_exists( dirname( __FILE__ ) . '/lib/Browser.php' ) ) {
+	require_once dirname( __FILE__ ) . '/lib/Browser.php';
+}
+
+/**
+ * IE?
+ *
+ * Default usage:
+ *
+ * if ( wds_browser_is_ie() ) {
+ *    <!-- some code here -->
+ * }
+ *
+ * Detect version:
+ *
+ * if ( wds_browser_is_ie( '11' ) ) {
+ *    <!-- some code here -->
+ * }
+ *
+ * @param  string   $version  The browser version.
+ * @return boolean
+ */
+function wds_browser_is_ie( $version = '' ) {
+
+	$browser = new Browser();
+
+	// If we're not passing the $version, detect the browser
+	if ( empty( $version ) && $browser->getBrowser() == Browser::BROWSER_IE ) {
+		return true;
+	}
+
+	// If we are passing the $version, detect the browser and version
+	if ( intval( $version ) >= $browser->getVersion() && $browser->getBrowser() == Browser::BROWSER_IE ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Chrome?
+ *
+ * Default usage:
+ *
+ * if ( wds_browser_is_chrome() ) {
+ *    <!-- some code here -->
+ * }
+ *
+ * Detect version:
+ *
+ * if ( wds_browser_is_chrome( '43' ) ) {
+ *    <!-- some code here -->
+ * }
+ *
+ * @param  string   $version  The browser version.
+ * @return boolean
+ */
+function wds_browser_is_chrome( $version = '' ) {
+
+	$browser = new Browser();
+
+	// If we're not passing the $version, detect the browser
+	if ( empty( $version ) && $browser->getBrowser() == Browser::BROWSER_CHROME ) {
+		return true;
+	}
+
+	// If we are passing the $version, detect the browser and version
+	if ( intval( $version ) >= $browser->getVersion() && $browser->getBrowser() == Browser::BROWSER_CHROME ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Safari?
+ *
+ * Default usage:
+ *
+ * if ( wds_browser_is_safari() ) {
+ *    <!-- some code here -->
+ * }
+ *
+ * Detect version:
+ *
+ * if ( wds_browser_is_safari( '8' ) ) {
+ *    <!-- some code here -->
+ * }
+ *
+ * @param  string   $version  The browser version.
+ * @return boolean
+ */
+function wds_browser_is_safari( $version = '' ) {
+
+	$browser = new Browser();
+
+	// If we're not passing the $version, detect the browser
+	if ( empty( $version ) && $browser->getBrowser() == Browser::BROWSER_SAFARI ) {
+		return true;
+	}
+
+	// If we are passing the $version, detect the browser and version
+	if ( intval( $version ) >= $browser->getVersion() && $browser->getBrowser() == Browser::BROWSER_SAFARI ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Firefox?
+ *
+ * Default usage:
+ *
+ * if ( wds_browser_is_firefox() ) {
+ *    <!-- some code here -->
+ * }
+ *
+ * Detect version:
+ *
+ * if ( wds_browser_is_firefox( '38' ) ) {
+ *    <!-- some code here -->
+ * }
+ *
+ * @param  string   $version  The browser version.
+ * @return boolean
+ */
+function wds_browser_is_firefox( $version = '' ) {
+
+	$browser = new Browser();
+
+	// If we're not passing the $version, detect the browser
+	if ( empty( $version ) && $browser->getBrowser() == Browser::BROWSER_FIREFOX ) {
+		return true;
+	}
+
+	// If we are passing the $version, detect the browser and version
+	if ( intval( $version ) >= $browser->getVersion() && $browser->getBrowser() == Browser::BROWSER_FIREFOX ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Mobile phones.
+ */
+function wds_browser_is_mobile() {
+
+	$browser = new Browser();
+
+	if ( true == $browser->isMobile() ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Tablets.
+ */
+function wds_browser_is_tablet() {
+
+	$browser = new Browser();
+
+	if ( true == $browser->isTablet() ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Mobile phone and/or tablet.
+ */
+function wds_browser_is_handheld() {
+
+	$browser = new Browser();
+
+	if ( true == $browser->isMobile() || true == $browser->isTablet() ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Bots.
+ */
+function wds_browser_is_robot() {
+
+	$browser = new Browser();
+
+	if ( true == $browser->isRobot() ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Facebook app.
+ */
+function wds_browser_is_facebook() {
+
+	$browser = new Browser();
+
+	if ( true == $browser->isFacebook() ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * AOL.
+ */
+function wds_browser_is_aol() {
+
+	$browser = new Browser();
+
+	if ( true == $browser->isAol() ) {
+		return true;
+	}
+
+	return false;
+}
+
+
+/**
+ * Windows OS.
+ */
+function wds_browser_is_windows() {
+
+	$browser = new Browser();
+
+	if ( 'Windows' === $browser->getPlatform() ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * OSX.
+ */
+function wds_browser_is_osx() {
+
+	$browser = new Browser();
+
+	if ( 'Apple' === $browser->getPlatform() ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Linux.
+ */
+function wds_browser_is_linux() {
+
+	$browser = new Browser();
+
+	if ( 'Linux' === $browser->getPlatform() ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * iOS.
+ */
+function wds_browser_is_ios() {
+
+	$browser = new Browser();
+
+	if ( 'iPhone' === $browser->getPlatform() || 'iPad' === $browser->getPlatform() || 'iPod' === $browser->getPlatform() ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Android
+ */
+function wds_browser_is_android() {
+
+	$browser = new Browser();
+
+	if ( 'Android' === $browser->getPlatform() ) {
+		return true;
+	}
+
+	return false;
+}
